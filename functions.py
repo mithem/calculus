766347e51b0fe64f54561923d8fdaa5fd4da0727
mithem <mithem@github.com>
2021-05-15 15:53:05 +0200

    Major improvements
    - refactore the different files into one (don't ask!)
    - Add FunctionSum: multiple functions added together
    - Bug fixes
    - Implement more derivative functions (for trig functions)
    - compute taylor polynomial for any function offering enough derivatives
    - (a little) more flexibility with configuring matplotlib + micro-cli
    (100% not staying)

---
+++ functions.py
@@ -0,0 +1,329 @@
+from typing import List, Union
+import numpy as np
+
+_standard_h = 0.001
+
+class Function:
+
+    def evaluate(self, x: float) -> float:
+        raise NotImplementedError()
+
+    def h_method(self, x: float, h: Union[None, float] = None) -> float:
+        if h is None:
+            h = _standard_h
+        hh = h / 2.0
+        dy = self.evaluate(x + hh) - self.evaluate(x - hh)
+        return dy / h
+
+    def get_nth_derivative(self, n: int):
+        if n == 0:
+            return self
+        try:
+            d = self.get_derivative()
+            return d.get_nth_derivative(n - 1)
+        except TypeError:
+            return None
+
+    def get_nth_indefinite_integral(self, n: int):
+        if n == 0:
+            return self
+        try:
+            return self.get_indefinite_integral().get_nth_indefinite_integral(n - 1)
+        except TypeError:
+            return None
+
+    def get_taylor_polynomial(self, n: int, x: float = 0):
+        constants = {}
+        for i in range(n + 1):
+            d = self.get_nth_derivative(i)
+            constants[i] = d.evaluate(x) / np.math.factorial(i)
+        return Polynomial(constants)
+
+class FunctionAddition(Function):
+    """Two functions added together"""
+    f1: Function
+    f2: Function
+
+    def evaluate(self, x: float) -> float:
+        try:
+            return self.f1.evaluate(x) + self.f2.evaluate(x)
+        except TypeError:
+            return None
+    # h_method doesn't have to be overridden as slope can be derived from overridden `.evaluate`
+
+    def __init__(self, f1: Function, f2: Function):
+        self.f1 = f1
+        self.f2 = f2
+
+class FunctionSum(Function):
+    """Many functions added together"""
+    functions: list[Function]
+
+    def evaluate(self, x: float) -> float:
+        try:
+            s = 0
+            for f in self.functions:
+                s += f.evaluate(x)
+            return s
+        except TypeError:
+            return None
+    
+    def get_derivative(self):
+        return self.__init__([f.get_derivative() for f in self.functions])
+
+    def get_indefinite_integral(self):
+        return self.__init__([f.get_indefinite_integral() for f in self.functions])
+
+    def __init__(self, functions: list[Function]):
+        self.functions = functions
+
+    def __str__(self):
+        return f"<FunctionSum([{ ', '.join([str(f) for f in self.functions]) }])>"
+
+class FunctionMultiplication(Function):
+    """Two functions multiplied with each other"""
+    f1: Function
+    f2: Function
+
+    def evaluate(self, x: float) -> float:
+        try:
+            return self.f1.evaluate(x) * self.f2.evaluate(x)
+        except TypeError:
+            return None
+    # h_method doesn't have to be overridden as slope can be derived from overridden `.evaluate`
+
+    def __init__(self, f1: Union[float, Function], f2: Union[float, Function]):
+        if issubclass(type(f1), Function):
+            self.f1 = f1
+        else:
+            self.f1 = Constant(f1)
+        if issubclass(type(f2), Function):
+            self.f2 = f2
+        else:
+            self.f2 = Constant(f2)
+
+    def get_indefinite_integral(self):
+        """S = Integral
+        S(u * v) = [U * v] - S(U * v') or
+        S(u * v) = [u * V] - S(u' * V)
+        """
+        try:
+            return FunctionAddition(self.__init__(self.f1.get_indefinite_integral(), self.f2), self.__init__(Polynomial([-1]), self.__init__(self.f1.get_indefinite_integral(), self.f2.get_derivative()).get_indefinite_integral()))
+        except Exception as e:
+            print(e + " (no worries for now)")
+            return FunctionAddition(self.__init__(self.f1, self.f2.get_indefinite_integral()), self.__init__(Polynomial([-1]), self.__init__(self.f1.get_derivative(), self.f2.get_indefinite_integral()).get_indefinite_integral()))
+
+    def get_derivative(self):
+        if issubclass(type(self.f1), Constant):
+            if self.f1.constant == -1:
+                if type(self.f2) == sin:
+                    return FunctionMultiplication(-1, cos())
+                elif type(self.f2) == cos:
+                    return sin()
+            elif self.f1.constant == 1:
+                return self.f2.get_derivative()
+        elif issubclass(type(self.f2), Constant):
+            if self.f2.constant == -1:
+                if type(self.f1) == sin:
+                    return FunctionMultiplication(-1, cos())
+                elif type(self.f1) == cos:
+                    return sin()
+            elif self.f2.constant == 1:
+                return self.f1.get_derivative()
+        else:
+            return FunctionSum(FunctionMultiplication(self.f1.get_derivative(), self.f2), FunctionMultiplication(self.f1, self.f2.get_derivative()))
+
+    def __str__(self):
+        return f"<FunctionMult(f1: {self.f1}, f2: {self.f2})>"
+
+class Constant(Function):
+    constant: float
+
+    def evaluate(self, x: float) -> float:
+        return self.constant
+
+    def __init__(self, constant: float):
+        self.constant = constant
+
+    def get_derivative(self):
+        return self.__init__(0)
+
+    def get_indefinite_integral(self):
+        return Polynomial({1: self.constant})
+
+    def __str__(self):
+        return f"<Constant({self.constant})>"
+
+class Linear(Function):
+    constant: float
+    
+    def evaluate(self, x:float) -> float:
+        return self.constant * x
+
+    def __init__(self, constant: float):
+        self.constant = constant
+
+    def get_derivative(self):
+        return Constant(self.constant)
+
+    def get_indefinite_integral(self):
+        return Polynomial({2: self.constant / 2})
+
+    def __str__(self):
+        return f"<Linear({self.constant}x)>"
+
+class Polynomial(Function):
+    """Examples:
+    `constants = {
+        -2: 1,
+        -1: 3,
+        0: 5,
+        1: -1.5,
+        2: 0.5
+    }`
+    => x⁻² + 3x⁻¹ + 5x⁰ -1.5x¹ + 0.5x²
+
+    `constants = {
+        -3: -2,
+        0: 1,
+        2: 2
+    }
+    `
+    => -2x⁻³ + 1 + 2x²
+    """
+    constants: dict[int, float]
+
+    def evaluate(self, x: float) -> float:
+        try:
+            s = 0
+            for key, value in self.constants.items():
+                s += value * x ** key
+            return s
+        except Exception:
+            return None
+
+    def __init__(self, constants: dict[int, float]):
+        self.constants = {}
+        for key in sorted(constants):
+            self.constants[key] = constants[key]
+
+    def get_derivative(self):
+        constants = self.constants.copy()
+        keys = list(constants.keys())
+        for i in range(len(constants)):
+            tmp = constants[keys[i]]
+            del constants[keys[i]]
+            if not keys[i] == 0: # f(x)' = (g(x) + c)' = g(x)'
+                constants[keys[i] - 1] = tmp * keys[i]
+        return Polynomial(constants)
+
+    def get_indefinite_integral(self):
+        constants = self.constants.copy()
+        keys = list(constants.keys())
+        ln = None
+        for i in range(len(constants)):
+            tmp = constants[keys[i]]
+            if keys[i] == -1:
+                ln =  FunctionMultiplication(natural_log(), constants[keys[i]])
+            del constants[keys[i]]
+            constants[keys[i] + 1] = tmp * keys[i]
+        if ln is None:
+            return Polynomial(constants)
+        return FunctionAddition(Polynomial(constants), ln)
+
+class PolynomialAddition(FunctionAddition):
+    f1: Polynomial
+    f2: Polynomial
+
+    def __init__(self, f1: Polynomial, f2: Polynomial):
+        self.f1 = f1
+        self.f2 = f2
+
+    def get_derivative(self):
+        return self.__init__(f1.get_derivative(), f2.get_derivative())
+
+    def get_indefinite_integral(self):
+        return self.__init__(f1.get_indefinite_integral(), f2.get_indefinite_integral())
+
+    def get_nth_derivative(self, n: int):
+        return self.__init__(f1.get_nth_derivative(n), f2.get_nth_derivative(n))
+
+    def get_nth_indefinite_integral(self, n: int):
+        return self.__init__(f1.get_nth_indefinite_integral(n), f2.get_nth_indefinite_integral(n))
+
+class PolynomialMultiplication(FunctionMultiplication):
+    f1: Polynomial
+    f2: Polynomial
+
+    def __init__(self, f1: Polynomial, f2: Polynomial):
+        self.f1 = f1
+        self.f2 = f2
+
+    def get_derivative(self):
+        """(u * v)' = u' * v + u * v'"""
+        return PolynomialAddition(self.__init__(f1.get_derivative(), f2), self.__init__(f1, f2.get_derivative()))
+
+class e_function(Function):
+    """e^x, where x is a number"""
+
+    def evaluate(self, x: float):
+        return pow(np.e, x)
+
+    def get_derivative(self):
+        return self
+
+class natural_log(Function):
+    """ln(x), where x is a number"""
+
+    def evaluate(self, x: float):
+        try:
+            return np.math.log(x)
+        except ValueError as e:
+            if "math domain error" in str(e):
+                return None
+
+    def get_derivative(self):
+        return Polynomial({-1: 1})
+
+    def get_indefinite_integral(self, n: int):
+        if n > 1:
+            raise NotImplementedError()
+        if n == 0:
+            return self
+        return self.get_derivative().get_nth_indefinite_integral(n - 1)
+
+class sin(Function):
+
+    def evaluate(self, x: float) -> float:
+        return np.math.sin(x)
+
+    def get_derivative(self):
+        return cos()
+
+    def get_indefinite_integral(self):
+        return FunctionMultiplication(-1, cos())
+
+    def __str__(self):
+        return "sin(x)"
+
+class cos(Function):
+
+    def evaluate(self, x: float) -> float:
+        return np.math.cos(x)
+
+    def get_derivative(self):
+        return FunctionMultiplication(-1, sin())
+
+    def get_indefinite_integral(self):
+        return sin()
+
+    def __str__(self):
+        return "cos(x)"
+
+class tan(Function):
+    
+    def evaluate(self, x: float) -> float:
+        return np.math.tan(x)
+
+    def __str__(self):
+        return "tan(x)"
--- run.py
+++ run.py
@@ -1,55 +1,70 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import functions
-from functions import Polynomial
-from functions.trig_functions import sin, cos
+import sys
+import time
+from functions import Polynomial, Constant, sin, cos
 
-min = -15
-max = -min
-delta = 0.01
+def evaluate(f1, f2, min_x: float, max_x: float = None, delta: float = None):
+    if max_x is None:
+        max_x = -min_x
+    if delta is None:
+        delta = 0.01
 
-np_a = list(np.arange(min, max, delta))
+    np_a = list(np.arange(min_x, max_x, delta))
+    x_values = []
+    for i in np_a:
+        x_values.append(float(i))
 
-x_values = []
+    l1 = [f1.evaluate(x) for x in x_values]
+    l2 = [f2.evaluate(x) for x in x_values]
 
-for i in np_a:
-    x_values.append(float(i))
+    x_1 = x_values.copy()
+    x_2 = x_values.copy()
 
-f = Polynomial({-1: 1})
+    n = 0
+    for i in range(len(x_1)):
+        if l1[i - n] != None:
+            continue
+        del l1[i - n]
+        del x_1[i - n]
+        n += 1
 
-f2 = f.get_nth_indefinite_integral(1)
+    n = 0
+    for i in range(len(x_2)):
+        if l2[i - n] != None:
+            continue
+        del l2[i - n]
+        del x_2[i - n]
+        n += 1
+
+    min_y = np.min(l1)
+    max_y = np.max(l1)
+
+    lim_min_y = min_y - np.abs(min_y) * 0.15
+    lim_max_y = max_y + np.abs(max_y) * 0.15
+
+    axes = plt.gca()
+    axes.set_ylim([lim_min_y, lim_max_y])
+
+    plt.plot(x_1, l1, label="f(x)") # for this example, you'll need to configure the matplotlib window to use sensible y-axis marks
+    plt.plot(x_2, l2, label="f'(x)")
+
+    plt.legend()
+    plt.show()
 
 try:
-    print(f2.constants)
+    n = int(sys.argv[1])
+    min_x = float(sys.argv[2])
 except:
-    pass
+    print("Expected 'python3 run.py n min_x' where n is the amount of derivatives (incl. 0) used to compute the tailor polynomial and min_x the minimum x value computed (max is symmetrical to y-axis)")
+    exit(-1)
 
-l1 = [f.evaluate(x) for x in x_values]
-l2 = [f2.evaluate(x) for x in x_values]
+f1 = cos()
 
-x_1 = x_values.copy()
-x_2 = x_values.copy()
+t1 = time.time()
+f2 = f1.get_taylor_polynomial(n)
+t2 = time.time()
 
-n = 0
-for i in range(len(x_1)):
-    if l1[i - n] != None:
-        continue
-    del l1[i - n]
-    del x_1[i - n]
-    n += 1
+print(f"It took {t2 - t1} seconds to compute the taylor polynomial.")
 
-n = 0
-for i in range(len(x_2)):
-    if l2[i - n] != None:
-        continue
-    del l1[i - n]
-    del l2[i - n]
-    del x_2[i - n]
-    n += 1
-
-plt.scatter(x_1, l1, label="f(x)") # for this example, you'll need to configure the matplotlib window to use sensible y-axis marks
-plt.scatter(x_2, l2, label="f'(x)")
-
-plt.legend()
-plt.show()
-
+evaluate(f1, f2, min_x)
